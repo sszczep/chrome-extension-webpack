1baaa558c6cdc4cfa4e60f2959821032465a2e58
NikkelM <nikkel.mollenhauer@gmx.de>
2023-05-23 19:47:43 +0200

    Added test command, dependencies, files

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common.cjs');
 
 module.exports = merge(common, {
   mode: 'production',
